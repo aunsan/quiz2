af77cf183068b23292b1115dfb299fed5df90108
Aung San Oo <ooa@student.douglascollege.ca>
2024-03-28 17:46:01 -0700

    cors added

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const mongoose = require('mongoose');
-
+const cors = require('cors');
 const express = require('express')
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
